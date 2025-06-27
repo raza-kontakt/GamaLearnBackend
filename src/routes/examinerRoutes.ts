57dc074ae14917f54beeb40c109740839f345c70
Ali Raza <alirazamunir401@gmail.com>
2025-06-27 04:28:32 +0200

    Add auth middleware

--- src/routes/examinerRoutes.ts
+++ src/routes/examinerRoutes.ts
@@ -1,14 +1,17 @@
-import { Router } from 'express';
-import { loginController, logoutController } from '../controllers/examinerController';
-import { validate } from '../utils/validation';
-import { loginSchema } from '../schemas/examinerSchema';
-import { authMiddleware } from '../middleware/authMiddleware';
-import { getExaminerController } from '../controllers/examinerController';
+import { Router } from "express";
+import {
+  loginController,
+  logoutController,
+} from "../controllers/examinerController";
+import { validate } from "../utils/validation";
+import { loginSchema } from "../schemas/examinerSchema";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { getExaminerController } from "../controllers/examinerController";
 
 const router = Router();
 
-router.post('/login', validate(loginSchema), loginController);
-router.post('/logout', logoutController);
-router.get('/me', authMiddleware, getExaminerController);
+router.post("/login", validate(loginSchema), loginController);
+router.post("/logout", authMiddleware, logoutController);
+router.get("/me", authMiddleware, getExaminerController);
 
 export default router;
